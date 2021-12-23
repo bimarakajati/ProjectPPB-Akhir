8e47ee304d8d0e1f563bf1cc3f215c76ecafaa86
Lucky Alma Aficionado Rigel <luckyrigel9802@gmail.com>
2021-12-24 00:25:28 +0800

    up-to_date

--- app/src/main/java/com/example/myportifolio/MainMenuActivity.java
+++ app/src/main/java/com/example/myportifolio/MainMenuActivity.java
@@ -6,12 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainMenuActivity extends AppCompatActivity {
 
     ImageView background;
+    GridLayout menu;
     float v = 0;
 
     @Override
@@ -20,9 +22,16 @@ public class MainMenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
 
         background = findViewById(R.id.bg);
+        menu = findViewById(R.id.grid);
+
         background.setTranslationY(800);
+        menu.setTranslationY(800);
+
         background.setAlpha(v);
+        menu.setAlpha(v);
+
         background.animate().translationY(0).alpha(1).setDuration(800).setStartDelay(300).start();
+        menu.animate().translationY(0).alpha(1).setDuration(800).setStartDelay(500).start();
 
 
 
