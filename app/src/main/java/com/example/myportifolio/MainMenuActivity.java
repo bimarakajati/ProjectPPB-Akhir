feb25319dacade13d777369073efad0a073f286c
Lucky Alma Aficionado Rigel <luckyrigel9802@gmail.com>
2021-12-24 00:08:06 +0800

    new bima

--- app/src/main/java/com/example/myportifolio/MainMenuActivity.java
+++ app/src/main/java/com/example/myportifolio/MainMenuActivity.java
@@ -2,7 +2,11 @@ package com.example.myportifolio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void education(View view) {
+        Intent i = new Intent(MainMenuActivity.this, iEducation.class);
+        startActivity(i);
+    }
+
+    public void calendar(View view) {
+        Intent i = new Intent(MainMenuActivity.this, iCalendar.class);
+        startActivity(i);
+    }
+
+    public void payment(View view) {
+        Intent i = new Intent(MainMenuActivity.this, iPayment.class);
+        startActivity(i);
+    }
+
+    public void about(View view) {
+        Intent i = new Intent(MainMenuActivity.this, iAbout.class);
+        startActivity(i);
     }
 }
