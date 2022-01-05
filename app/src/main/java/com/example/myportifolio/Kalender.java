24b01cf9aa560caa95b0a38e08baced2e1d5ace7
arviyonismara <arviyonismara@gmail.com>
2022-01-06 00:18:53 +0700

    Tambah Kalender

---
+++ app/src/main/java/com/example/myportifolio/Kalender.java
@@ -0,0 +1,80 @@
+package com.example.myportifolio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import org.naishadhparmar.zcustomcalendar.CustomCalendar;
+import org.naishadhparmar.zcustomcalendar.OnDateSelectedListener;
+import org.naishadhparmar.zcustomcalendar.Property;
+
+import java.util.Calendar;
+import java.util.HashMap;
+
+public class Kalender extends AppCompatActivity {
+    //inisialisasi
+    CustomCalendar customCalendar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kalender);
+
+        customCalendar = findViewById(R.id.custom_calendar);
+
+        HashMap<Object, Property> descHashMap = new HashMap<>();
+
+        Property defaultProperty = new Property();
+
+        defaultProperty.layoutResource = R.layout.default_view;
+
+        defaultProperty.dateTextViewResource = R.id.text_view;
+
+        descHashMap.put("default",defaultProperty);
+
+        Property currentProperty = new Property();
+        currentProperty.layoutResource = R.layout.current_view;
+        currentProperty.dateTextViewResource = R.id.text_view;
+        descHashMap.put("current",currentProperty);
+
+        Property presentProperty = new Property();
+        presentProperty.layoutResource = R.layout.present_view;
+        presentProperty.dateTextViewResource = R.id.text_view;
+        descHashMap.put("present",presentProperty);
+
+        Property absentProperty = new Property();
+        absentProperty.layoutResource = R.layout.absent_view;
+        absentProperty.dateTextViewResource = R.id.text_view;
+        descHashMap.put("absent",absentProperty);
+
+        customCalendar.setMapDescToProp(descHashMap);
+
+        HashMap<Integer,Object> dateHashMap = new  HashMap<>();
+
+        Calendar calendar = Calendar.getInstance();
+
+        dateHashMap.put(calendar.get(Calendar.DAY_OF_MONTH),"current");
+        dateHashMap.put(1,"present");
+        dateHashMap.put(1,"absent");
+        dateHashMap.put(3,"present");
+        dateHashMap.put(4,"absent");
+        dateHashMap.put(20,"present");
+        dateHashMap.put(30,"absent");
+
+        customCalendar.setDate(calendar,dateHashMap);
+
+        customCalendar.setOnDateSelectedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(View view, Calendar selectedDate, Object desc) {
+                String sDate = selectedDate.get(Calendar.DAY_OF_MONTH)
+                        +"/"+ (selectedDate.get(Calendar.MONTH) + 1)
+                        +"/"+ selectedDate.get(Calendar.YEAR);
+
+                Toast.makeText(getApplicationContext()
+                        ,sDate,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
