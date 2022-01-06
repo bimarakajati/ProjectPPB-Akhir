0d5cb5bd228aa261035fad7892d94a04308adff6
Lucky Alma Aficionado Rigel <luckyrigel9802@gmail.com>
2022-01-06 17:04:38 +0700

    terbaru

--- app/src/main/java/com/udinus/bimaeducation/ForumActivity.java
+++ app/src/main/java/com/udinus/bimaeducation/ForumActivity.java
@@ -8,6 +8,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ForumActivity extends AppCompatActivity {
     EditText nis, nama, pesan;
@@ -20,21 +21,21 @@ public class ForumActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forum);
 
-//        nis = findViewById(R.id.nis);
-//        nama = findViewById(R.id.nama);
-//        pesan = findViewById(R.id.pesan);
+        nis = findViewById(R.id.nis);
+        nama = findViewById(R.id.nama);
+        pesan = findViewById(R.id.pesan);
         tblAdd = findViewById(R.id.tblAdd);
-//        siswa = new Siswa();
-//        reff = FirebaseDatabase.getInstance().getReference().child("Siswa");
-//
-//        tblAdd.setOnClickListener(view -> {
-//            siswa.setNis(nis.getText().toString().trim());
-//            siswa.setNama(nama.getText().toString().trim());
-//            siswa.setPesan(pesan.getText().toString().trim());
-//            reff.push().setValue(siswa);
-//            Toast.makeText(ForumActivity.this, "Data tersimpan", Toast.LENGTH_LONG).show();
-//        });
+        siswa = new Siswa();
+        reff = FirebaseDatabase.getInstance().getReference().child("Siswa");
 
-        tblAdd.setOnClickListener(view -> Toast.makeText(ForumActivity.this, "Data tersimpan", Toast.LENGTH_LONG).show());
+        tblAdd.setOnClickListener(view -> {
+            siswa.setNis(nis.getText().toString().trim());
+            siswa.setNama(nama.getText().toString().trim());
+            siswa.setPesan(pesan.getText().toString().trim());
+            reff.push().setValue(siswa);
+            Toast.makeText(ForumActivity.this, "Data tersimpan", Toast.LENGTH_LONG).show();
+        });
+
+//        tblAdd.setOnClickListener(view -> Toast.makeText(ForumActivity.this, "Data tersimpan", Toast.LENGTH_LONG).show());
     }
 }
